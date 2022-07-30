71b32ed4b4c945cf49dae30e018a20784b475dd4
Miao Zhao <miao_zhao@yahoo.com>
2022-07-30 10:50:51 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import "./App.css";
-import ToDoItem from "./components/toDoItem";
-import ToDoForm from "./components/toDoForm";
+import ToDoItem from "./components/toDoItem/toDoItem";
+import ToDoForm from "./components/toDoForm/toDoForm";
 
 function App() {
   const [todos, setTodos] = useState(["a", "b", "c", "d", "e", "f"]);
   console.log("todos ...", todos);
   return (
-    <div>
-      <h1>My todo list</h1>
-      {todos.map((item, index) => {
-        return <ToDoItem key={index} item={item} />;
-      })}
-      <ToDoForm todos={todos} setTodos={setTodos} />
+    <div className="app">
+      <div className="todo-list">
+        <h1>My todo list</h1>
+        {todos.map((item, index) => {
+          return <ToDoItem key={index} item={item} />;
+        })}
+        <ToDoForm todos={todos} setTodos={setTodos} />
+      </div>
     </div>
   );
 }
